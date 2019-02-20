6392648f174b974afa47df25bc5aa56474298b90
ronfletcher1 <ronfletcher1@me.com>
2019-02-19 20:59:46 -0500

    first commit front-end for To-Do List

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './NavBar';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import axios from 'axios'
+
+class App extends Component {
+
+  addNewTask(task, date){
+    console.dir(task, date);
+    axios({
+      method: 'Post',
+      url: 'http://localhost:3000/addTask',
+      data: {
+        taskName: task,
+        taskDate: date
+      }
+    }).then((backEndResponse)=>{
+      console.log(backEndResponse)
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+      <div className="App">
+      <NavBar />
+      <Route exact path="/" render={()=>{
+        return (<Home addNewTask={this.addNewTask} />)
+      }} />
+        
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
